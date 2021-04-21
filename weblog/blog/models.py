54c91a1c89c2e4485435ccab6258c46c3fd6a7b9
charlie <sadegh.rez@gmail.com>
2021-04-22 01:20:54 +0430

    finish model

--- weblog/blog/models.py
+++ weblog/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Article(models.Model):
+    
+    STATUS = (
+        ('d','draft'),
+        ('p','publish')
+    )
+    title = models.CharField(max_length = 120)
+    slug = models.SlugField(max_length = 120, unique= True)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS, default='d')
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
