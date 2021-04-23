c3335d4d96d10075e08aca5f20cb6ce153c9ea6b
charlie <sadegh.rez@gmail.com>
2021-04-24 02:03:43 +0430

    create form widget

--- weblog/accounts/forms.py
+++ weblog/accounts/forms.py
@@ -2,6 +2,16 @@ from django import forms
 
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField(max_length = 30)
-    password = forms.CharField(max_length = 30)
+    username = forms.CharField(max_length = 30, widget = forms.TextInput(
+        attrs = {
+            'class' : 'form-control',
+            'placeholder' : 'Enter Username',
+        }
+    ))
+    password = forms.CharField(max_length = 30, widget=forms.PasswordInput( 
+        attrs = {
+            'class' : 'form-control',
+            'placeholder' : 'Enter password',
+        }
+     ))
 
