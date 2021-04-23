8218534534bc028079ed708aeb49e3518f9eccd3
charlie <sadegh.rez@gmail.com>
2021-04-23 16:28:59 +0430

    create base form

--- weblog/blog/urls.py
+++ weblog/blog/urls.py
@@ -5,5 +5,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.articles, name='home'),
-    path('<int:id>/<slug:slug>', views.the_article, name = "the_article"),
+    path('<int:id>/<slug:slug>', views.the_article, name="the_article"),
 ]
