d7928ba9dd2de53e4079c7f577ffbb2e65ba614e
charlie <sadegh.rez@gmail.com>
2021-05-01 02:07:31 +0430

    confirm password field

--- weblog/accounts/forms.py
+++ weblog/accounts/forms.py
@@ -31,7 +31,13 @@ class UserRegistrationForm(forms.Form):
             'placeholder' : 'your email',
         }
     ))
-    password = forms.CharField(max_length = 30, widget=forms.PasswordInput( 
+    password = forms.CharField(max_length = 30, label='password' , widget=forms.PasswordInput( 
+        attrs = {
+            'class' : 'form-control',
+            'placeholder' : 'your password',
+        }
+    ))
+    password2 = forms.CharField(max_length = 30, label='confirm password' , widget=forms.PasswordInput( 
         attrs = {
             'class' : 'form-control',
             'placeholder' : 'your password',
@@ -44,4 +50,12 @@ class UserRegistrationForm(forms.Form):
         if user.exists() :
             raise forms.ValidationError('this email Already exists.')
         return email
+
+    def clean_password(self):
+        p = self.cleaned_data['password']
+        p2 = self.cleaned_data['password2']
+        if p != p2 :
+
+            raise forms.ValidationError('password is not match')
+        return p
 
